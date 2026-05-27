3956ac10ba3eb82d73c49504eb25a3d712be3890
haliwt <jonewang@126.com>
2026-05-27 17:31:30 +0800

    update fan run is normal be set up timer timing value .DATA.2026-05-27

--- Bsp/src/bsp_usart.c
+++ Bsp/src/bsp_usart.c
@@ -585,9 +585,9 @@ static void parse_copy_cmd_or_data_handler(uint8_t *pdata)
 			 
 
 			}
-			else if(pdata[4]==0){
+			else if(pdata[4]==0 || pdata[4]==2){
 		
-           
+               LCD_Display_Wind_Icon_Handler();
 			   gpro_t.gTimer_power_off_on_minute_fan=0;
 			   run_t.power_on = power_off;
 
