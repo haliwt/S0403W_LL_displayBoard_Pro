5b3d8d239e71ce3d5de1881edc7c2e228d68ce2a
haliwt <jonewang@126.com>
2026-06-09 09:48:07 +0800

    update

--- Bsp/src/bsp.c
+++ Bsp/src/bsp.c
@@ -574,13 +574,13 @@ void direct_comparison_temp(void)
 
                run_t.dry = 0;
 			 
-			  SendData_Set_Command(0x22,0x00); //close ptc 
+			  SendData_Set_Command(0x23,0x00); //close ptc 
 	          tx_thread_sleep(1);
     }
     else{
 
 	   run_t.dry = 1;
-	    SendData_Set_Command(0x22,0x01); //open ptc 
+	    SendData_Set_Command(0x23,0x01); //open ptc 
 	   tx_thread_sleep(1);
 	  
 			    
